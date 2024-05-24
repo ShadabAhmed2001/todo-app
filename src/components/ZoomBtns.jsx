b7b92c0d5e3e1761de428a2217f926a5a23ce471
ShadabAhmed2001 <k.shadabahmed2001@gmail.com>
2024-05-24 14:17:59 +0530

    fixed calender button

--- src/components/ZoomBtns.jsx
+++ src/components/ZoomBtns.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, {  useState } from 'react';
 import "./style.css"
 import { GoZoomIn } from "react-icons/go";
 import { GoZoomOut } from "react-icons/go";
